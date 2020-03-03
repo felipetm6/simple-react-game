ddb6c4ebafdd9d6f4b2a202b7caa95d1195fb0b0
Felipe Teixeira Moraes <felipetm6@hotmail.com>
2020-03-03 16:51:02 -0300

    Implement Player component inside the game

--- src/components/Game/index.tsx
+++ src/components/Game/index.tsx
@@ -1,18 +1,15 @@
-import React, { useCallback, useEffect, useRef, useState } from "react";
+import React, { useCallback, useEffect, useRef } from "react";
 import { useGameView } from "../../hooks/gameHooks";
-import { imageLoader, importAllImages } from "../../utils/imageHelper";
-const imageResources: string[] = importAllImages(
-  require.context("images/sprites/boy/walk/", false, /\.(png|jpe?g|svg)$/)
-);
+import Player from "../Player";
+import { IDrawableComponent } from "../../types";
 
 const Game = () => {
-  const [images, setImages] = useState<HTMLImageElement[]>([]);
   const { width, height } = useGameView();
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
-  const currentFrame = useRef<number>(0);
+  const player = useRef<IDrawableComponent>(null);
 
   const draw = useCallback(() => {
-    if (!canvasRef.current || !images.length) {
+    if (!canvasRef.current) {
       return;
     }
     const context = canvasRef.current.getContext("2d");
@@ -23,30 +20,16 @@ const Game = () => {
 
     context.clearRect(0, 0, width, height);
 
-    context.drawImage(images[currentFrame.current], 0, 0);
-    currentFrame.current =
-      currentFrame.current < images.length - 1 ? currentFrame.current + 1 : 0;
-  }, [height, images, width]);
+    player.current!.draw(context);
+  }, [height, width]);
 
   useEffect(() => {
     setInterval(draw, 63);
   }, [draw]);
 
-  useEffect(() => {
-    const preloadImages = async () => {
-      const preloadedImages: HTMLImageElement[] = await Promise.all(
-        imageResources.map(path => imageLoader(path))
-      );
-
-      setImages(preloadedImages);
-    };
-
-    preloadImages();
-  }, []);
-
   return (
     <canvas ref={ref => (canvasRef.current = ref)} width={450} height={650}>
-      <p>a</p>
+      <Player gameRef={player} />
     </canvas>
   );
 };
